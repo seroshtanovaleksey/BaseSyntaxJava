2530f6358c4e8cace05003889eca4124d360ac29
Aleksey Seroshtanov <aseroshtanov1c@gmail.com>
2023-04-02 22:22:16 +0300

    commit 1 02.04.2023

---
+++ src/HomeWork.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("Как зовут вашего друга?");
+
+        Scanner in  = new Scanner(System.in);
+        String nameFriend = in.nextLine();
+
+        System.out.println("Сколько лет вашему другу?");
+        String ageFriend = in.nextLine();
+
+        System.out.println("Моему другу "+nameFriend+" на данный момент "+ageFriend+" лет");
+        System.out.println("тест");
+
+    }
+}
